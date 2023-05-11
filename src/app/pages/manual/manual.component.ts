36c045a97280ecc924dd1d44a79f672e6ed54275
radialserial <ntnbinary@gmail.com>
2023-05-10 22:00:38 -0300

    Added prismjs syntax highlighting

--- src/app/pages/manual/manual.component.ts
+++ src/app/pages/manual/manual.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { Page } from 'src/app/util/page';
 import { MANUAL_PAGES } from './manualPages';
+import { HighlightService } from 'src/app/util/prism.service';
 
 @Component({
   selector: 'app-manual',
   templateUrl: './manual.component.html',
-  styleUrls: ['./manual.component.css']
+  styleUrls: ['./manual.component.scss']
 })
 export class ManualComponent {
 
   selectedPageId: number = 0;
   manualPages: Page[] = MANUAL_PAGES;
+  highlighted: boolean = false;
 
-  constructor() { }
+  constructor(
+    private highlightService: HighlightService) {
+  }
+
+  ngAfterViewChecked() {
+    this.highlightService.highlightAll();
+    this.highlighted = true;
+  }
 
   selectPage(id: number) {
     this.selectedPageId = id;
